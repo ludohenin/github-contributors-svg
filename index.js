706ad70afd666903787d72f21267ec2a7b02fda5
ludohenin <ludovic.henin@yahoo.com>
2016-03-11 18:05:54 +0100

    chore: cleanup

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var fs = require('fs');
-var CloudConvert = require('cloudconvert');
-var contributors = require('github-contributors-list/lib/contributors');
-var table = require('github-contributors-list/lib/strategies/table');
-var marked = require('marked');
-var request = require('request');
 var express = require('express');
 var Webtask = require('webtask-tools');
 var getContributors = require('./contributors');
 
 var server = express();
-var options = {};
-options.filter = [];
 
 
 server.get('/:user/:repo.:ext', function (req, res) {
